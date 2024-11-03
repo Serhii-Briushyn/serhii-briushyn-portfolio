005bf8fc143eae5a098fd2afac345485097e4e12
Serhii Briushyn <serhii.briushyn@icloud.com>
2024-11-03 13:03:30 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,13 +9,13 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10 pt-32 pb-safe xl:py-36">
-        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto hide-scrollbar">
+        <div className="text-center flex flex-col xl:justify-center xl:pt-40 xl:text-left h-full container mx-auto hide-scrollbar">
           <motion.h1
             variants={fadeIn("down", 0.2)}
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="h1"
+            className="h1 max-xl:z-20"
           >
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
@@ -25,7 +25,7 @@ const Home = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16"
+            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16 max-xl:z-20"
           >
             Full-stack developer with expertise in creating dynamic,
             user-centric applications. Let&apos;s bring your vision to life
